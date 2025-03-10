d5537466732a17d0c16f1bdcbea43e02808a01da
Arpit1033 <lcb2022015@iiitl.ac.in>
2025-03-10 13:42:03 +0530

    Refactor frontend routing and add new container components for authentication and home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './containers/Home'
+import Login from './containers/Login'
+import Signup from './containers/Signup'
+import Activate from './containers/Activate'
+import ResetPassword from './containers/ResetPassword'
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm'
+import Layout from './hocs/Layout'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path='/' Component={Home}/>
+            <Route exact path='/login' Component={Login}/>
+            <Route exact path='/signup' Component={Signup}/>
+            <Route exact path='/reset-password' Component={ResetPassword}/>
+            <Route exact path='/password/reset/confirm/:uid/:token' Component={ResetPasswordConfirm}/>
+            <Route exact path='/activate/:uid/:token' Component={Activate}/>
+          </Routes>
+        </Layout>
+      </Router>
     </>
   )
 }
